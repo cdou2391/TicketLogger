d75650b9b56dd257d2ae74cc761b6e2ea260eeca
Cedric Rugamba <rugced@yahoo.fr>
2018-12-14 15:18:02 +0200

    Code Cleanup. Phase 1 Completed (i think).

--- TicketsLogger/TicketForm.cs
+++ TicketsLogger/TicketForm.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Net;
-using System.Net.Mail;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TicketsLogger
